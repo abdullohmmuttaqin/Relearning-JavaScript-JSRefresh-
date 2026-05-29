99f0e33e3e9ebedfc243f400fdd76a38f3300c20
abdullohmmuttaqin <abdullohmmuttaqin.09@mail.com>
2026-05-30 02:43:10 +0700

    Add learn values & type data

--- Nilai-dan-Tipe-Data-pada-Javascript/angka.js
+++ Nilai-dan-Tipe-Data-pada-Javascript/angka.js
@@ -1,3 +1,5 @@
+// bit/binary digit, adalah satuan terkecil dari data yang ada di dalam komputer kita //
+
 /* Number in Javascript are 
 double-precision Floating Point 
 64-bit format IEEE 754 values */
